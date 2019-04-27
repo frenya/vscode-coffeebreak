942dc67771f492b85b7dfe3bc8fde66f789b1a81
frenya <frantisek.vymazal@gmail.com>
2019-04-27 14:19:09 +0200

    Showing due date in description

--- src/views/items/todo.ts
+++ src/views/items/todo.ts
@@ -1,7 +1,16 @@
 
 /* IMPORT */
 
+var moment = require('moment');
+
 import Item from './item';
+
+const displayFormat = {
+  sameDay: '[Today]', 
+  nextDay: '[Tomorrow]', 
+  nextWeek: 'dddd', 
+  sameElse: 'dddd, MMM DD'
+};
 
 /* TODO */
 
@@ -13,7 +22,7 @@ class Todo extends Item {
 
     super ( obj, label );
 
-    this.tooltip = obj.code || obj.line;
+    this.tooltip = obj.message;
 
     this.command = {
       title: 'Reveal',
@@ -23,6 +32,13 @@ class Todo extends Item {
 
     if ( icon ) {
       this.setTypeIcon ( obj.type );
+    }
+
+    // console.log('Detecting date in', obj.line);
+    // Detect due date
+    let match = /[1-9][0-9]{3}-[0-9]{2}-[0-9]{2}/.exec(obj.line);
+    if (match && match.length) {
+      this.description = moment(match[0]).calendar(null, displayFormat);
     }
 
   }
