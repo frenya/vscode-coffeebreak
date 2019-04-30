ca17cfa2f6cdaf8ed4d5ff9889d5c2cc4cc5d949
frenya <frantisek.vymazal@gmail.com>
2019-04-30 16:39:25 +0200

    Imported Markdown Todo code

--- src/commands.ts
+++ src/commands.ts
@@ -6,6 +6,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import Config from './config';
+import Consts from './consts';
+import Editor from './editor';
 import ItemFile from './views/items/item';
 import ItemTodo from './views/items/todo';
 import Utils from './utils';
@@ -112,8 +114,71 @@ async function newFile () {
   });
 }
 
+/* TOGGLE RULES */
 
+async function toggleRules ( ...rules ) {
+
+  const textEditor = vscode.window.activeTextEditor;
+
+  if ( !Editor.isSupported ( textEditor ) ) return;
+
+  const textDocument = textEditor.document,
+        lines = _.uniq ( textEditor.selections.map ( selection => textDocument.lineAt ( selection.active.line ) ) );
+
+  if ( !lines.length ) return;
+
+  const edits = [];
+
+  lines.forEach ( line => {
+
+    rules.find ( ([ regex, replacement ]) => {
+
+      if ( !regex.test ( line.text ) ) return false;
+
+      const nextText = line.text.replace ( regex, replacement );
+
+      edits.push ( ..._.filter ( _.flattenDeep ( lines.map ( line => Editor.edits.makeDiff ( line.text, nextText, line.lineNumber ) ) ) ) );
+
+      return true;
+
+    });
+
+  });
+
+  if ( !edits.length ) return;
+
+  await Editor.edits.apply ( textEditor, edits );
+
+}
+
+/* COMMANDS */
+
+function toggleTodo () {
+
+  const {bullet} = Consts.symbols,
+        {line, todoBox, todoDone} = Consts.regexes;
+
+  toggleRules (
+    [todoBox, '$1$3'],
+    [todoDone, `$1${bullet} [ ] $3`],
+    [line, `$1${bullet} [ ] $3`]
+  );
+
+}
+
+function toggleDone () {
+
+  const {bullet, done} = Consts.symbols,
+        {line, todoBox, todoDone} = Consts.regexes;
+
+  toggleRules (
+    [todoDone, `$1${bullet} [ ] $3`],
+    [todoBox, `$1${bullet} [${done}] $3`],
+    [line, `$1${bullet} [${done}] $3`]
+  );
+
+}
 
 /* EXPORT */
 
-export { viewRevealTodo, viewEmbeddedCollapse, viewEmbeddedExpand, viewEmbeddedFilter, viewEmbeddedClearFilter, openTaskURL, newFile };
+export { viewRevealTodo, viewEmbeddedCollapse, viewEmbeddedExpand, viewEmbeddedFilter, viewEmbeddedClearFilter, openTaskURL, newFile, toggleTodo, toggleDone };
--- src/consts.ts
+++ src/consts.ts
@@ -7,7 +7,16 @@ import Config from './config';
 /* CONSTS */
 
 const Consts = {
+  languageId: 'markdown',
+  symbols: {
+    bullet: '-',
+    done: 'x'
+  },
   regexes: {
+    line: /^(\s*)([*+-]?\s*)(.*)$/,
+    todo: /^(\s*)([*+-]\s+\[[ xX]\]\s*)(.*)$/,
+    todoBox: /^(\s*)([*+-]\s+\[ \]\s*)(.*)$/,
+    todoDone: /^(\s*)([*+-]\s+\[[xX]\]\s*)(.*)$/,
     todoEmbedded: new RegExp ('(?:-\\s*\\[ \\])(?:( (?:@[^\\s]*)?))(.*)', 'g' ),
   }
 };
